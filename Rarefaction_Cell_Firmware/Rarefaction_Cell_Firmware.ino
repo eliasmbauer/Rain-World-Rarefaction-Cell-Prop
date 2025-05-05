46d9c1f5a2a780209d8484cb92da56024c60185b
Elias Bauer <eliasmbauer3@gmail.com>
2025-05-05 14:32:47 -0500

    added a firmware file with pin definitions, as well as slight modifications to the schematic

---
+++ Rarefaction_Cell_Firmware/Rarefaction_Cell_Firmware.ino
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+#include <FastLED.h>
+#include <DFRobot_MAX98357A.h>
+
+#define LED_COM 33
+#define OPEN_PIN_1 42
+#define OPEN_PIN_2 45
+#define BAT_INDICATOR_1 36
+#define BAT_INDICATOR_2 37
+#define BAT_INDICATOR_3 38
+#define AUDIO_BCLK 4
+#define AUDIO_LRCK 5
+#define AUDIO_DOUT 6
+#define AUDIO_GAIN 7
+
+void setup() 
+{
+
+}
+
+void loop() 
+{
+
+}
